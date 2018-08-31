fdc3f18d038560960dd2d8e52ca50c4a38d196f1
Zaw <zaw.sfo@gmail.com>
2018-08-30 22:10:40 -0700

    update

--- FSharpForms/App.fs
+++ FSharpForms/App.fs
@@ -5,8 +5,19 @@ open Xamarin.Forms
 type App() =
     inherit Application()
 
+    let mutable count:int = 1
+
     let stack = StackLayout(VerticalOptions = LayoutOptions.Center)
     let label = Label(XAlign = TextAlignment.Center, Text = "Welcome to F# Xamarin.Forms!")
+    let button = Button(Text="Hello World, Click Me!")
     do
         stack.Children.Add(label)
+        stack.Children.Add(button)
         base.MainPage <- ContentPage(Content = stack)
+        
+        button.Clicked.Add(fun args -> 
+            button.Text <- sprintf "%d clicks!" count
+            count <- count + 1
+        )
+    
+
