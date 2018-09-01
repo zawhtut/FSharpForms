419bcb2d57ca2ab1e306ef17c5ac99dd612c4b5e
Zaw <zaw.sfo@gmail.com>
2018-08-31 17:40:56 -0700

    Updated with Web Services

--- FSharpForms/App.fs
+++ FSharpForms/App.fs
@@ -1,23 +1,33 @@
 ﻿namespace FSharpForms
 
+open System
 open Xamarin.Forms
+open FSharp.Data
+
+type Company = JsonProvider<"https://autocomplete.clearbit.com/v1/companies/suggest?query=microsoft">
 
 type App() =
-    inherit Application()
+  inherit Application()
 
-    let mutable count:int = 1
+  let stack = StackLayout(VerticalOptions = LayoutOptions.Start, Margin = Thickness(15., 30.))
+  let input = Entry(Placeholder = "Enter a company name")
+  let label = Label(XAlign = TextAlignment.Center, Text = "Welcome to F# Xamarin.Forms!")
+  let image = Image(Aspect = Aspect.AspectFit)
 
-    let stack = StackLayout(VerticalOptions = LayoutOptions.Center)
-    let label = Label(XAlign = TextAlignment.Center, Text = "Welcome to F# Xamarin.Forms!")
-    let button = Button(Text="Hello World, Click Me!")
-    do
-        stack.Children.Add(label)
-        stack.Children.Add(button)
-        base.MainPage <- ContentPage(Content = stack)
-        
-        button.Clicked.Add(fun args -> 
-            button.Text <- sprintf "%d clicks!" count
-            count <- count + 1
-        )
-    
+  let search() = async {
+    let url = sprintf "https://autocomplete.clearbit.com/v1/companies/suggest?query=%s" (Uri.EscapeUriString input.Text)
+    let! json = Http.AsyncRequestString url
+    let company = Company.Parse json |> Seq.head
 
+    label.Text <- company.Name
+    image.Source <- ImageSource.FromUri (Uri company.Logo)
+  }
+
+  do
+    stack.Children.Add input
+    stack.Children.Add label
+    stack.Children.Add image
+    base.MainPage <- ContentPage(Content = stack)
+
+    input.Completed.Add (fun _ -> 
+      search() |> Async.StartImmediate)
